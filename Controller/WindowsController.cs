f12b3c5b79140ece7a6dbc570aa8923ff600656b
bayushi-yakusoku <bayushi.tonio@gmail.com>
2021-09-18 17:39:41 +0200

    Reorg folders

---
+++ Controller/WindowsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterReminder.Sample
+{
+    class WindowsController
+    {
+        private static Lazy<WindowsController> lazy = new(() => new());
+        private static WindowsController Instance => lazy.Value;
+
+        private WindowsController()
+        {
+            Debug.WriteLine($"WindowsController: Private Constructor");
+        }
+
+    }
+}
